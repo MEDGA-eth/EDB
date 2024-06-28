021fec579e96f50aab65649dbd74929486367295
Zhuo Zhang <zhan3299@purdue.edu>
2024-06-27 22:42:43 -0500

    test: add test for ASTPruner

--- crates/debug-backend/src/analysis/prune.rs
+++ crates/debug-backend/src/analysis/prune.rs
@@ -131,3 +131,84 @@ impl ASTPruner {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{path::PathBuf, str::FromStr, time::Duration};
+
+    use alloy_chains::Chain;
+    use alloy_primitives::Address;
+    use eyre::Result;
+    use foundry_block_explorers::Client;
+    use foundry_compilers::{
+        artifacts::{output_selection::OutputSelection, SolcInput, Source},
+        solc::{Solc, SolcLanguage},
+    };
+    use serial_test::serial;
+
+    use crate::etherscan_rate_limit_guard;
+
+    use super::*;
+
+    async fn download_and_compile(chain: Chain, addr: Address) -> Result<()> {
+        let cache_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("testdata")
+            .join("etherscan_cache")
+            .join(chain.to_string());
+        let cache_ttl = Duration::from_secs(u32::MAX as u64); // we don't want the cache to expire
+        let client =
+            Client::builder().chain(chain)?.with_cache(Some(cache_root), cache_ttl).build()?;
+
+        // download the source code
+        let mut meta = etherscan_rate_limit_guard!(client.contract_source_code(addr).await)?;
+        eyre::ensure!(meta.items.len() == 1, "contract not found or ill-formed");
+        let meta = meta.items.remove(0);
+
+        // prepare the input for solc
+        let mut settings = meta.settings()?;
+        // enforce compiler output all possible outputs
+        settings.output_selection = OutputSelection::complete_output_selection();
+        let sources =
+            meta.sources().into_iter().map(|(k, v)| (k.into(), Source::new(v.content))).collect();
+        let input = SolcInput::new(SolcLanguage::Solidity, sources, settings);
+
+        // prepare the compiler
+        let version = meta.compiler_version()?;
+        let compiler = Solc::find_or_install(&version)?;
+
+        let mut output = compiler.compile_exact(&input)?;
+        for (_, contract) in output.sources.iter_mut() {
+            ASTPruner::convert(contract.ast.as_mut().ok_or(eyre!("AST does not exist"))?)?;
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_solidity_v0_7_6() {
+        let addr = Address::from_str("0x1f98431c8ad98523631ae4a59f267346ea31f984").unwrap();
+        download_and_compile(Chain::default(), addr).await.unwrap();
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_solidity_v0_6_12() {
+        let addr = Address::from_str("0x1eb4cf3a948e7d72a198fe073ccb8c7a948cd853").unwrap();
+        download_and_compile(Chain::default(), addr).await.unwrap();
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_solidity_v0_5_17() {
+        let addr = Address::from_str("0xee39E4A6820FFc4eDaA80fD3b5A59788D515832b").unwrap();
+        download_and_compile(Chain::default(), addr).await.unwrap();
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_solidity_v0_4_24() {
+        let addr = Address::from_str("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48").unwrap();
+        download_and_compile(Chain::default(), addr).await.unwrap();
+    }
+}
